47ce17862b0f113080c0291e867007b9614ccba3
Max Weinzierl <supermaxman1@gmail.com>
2023-12-01 23:03:25 -0600

    Updated predict

--- llava/eval/model_vqa_med_predict.py
+++ llava/eval/model_vqa_med_predict.py
@@ -7,15 +7,80 @@ from tqdm import tqdm
 from datasets import load_dataset
 
 from llava import LlavaLlamaForCausalLM
+        from transformers import CLIPImageProcessor, CLIPVisionModel
 from llava.conversation import conv_templates
 from llava.utils import disable_torch_init
-from transformers import CLIPImageProcessor, StoppingCriteria
+from transformers import StoppingCriteria, BitsAndBytesConfig
 
 
 DEFAULT_IMAGE_TOKEN = "<image>"
 DEFAULT_IMAGE_PATCH_TOKEN = "<im_patch>"
 DEFAULT_IM_START_TOKEN = "<im_start>"
 DEFAULT_IM_END_TOKEN = "<im_end>"
+
+
+def load_pretrained_model(
+    model_name,
+    load_8bit=False,
+    load_4bit=False,
+    load_bf16=False,
+    device_map="auto",
+    device="cuda",
+):
+    kwargs = {"device_map": device_map}
+
+    if device != "cuda":
+        kwargs["device_map"] = {"": device}
+
+    if load_8bit:
+        kwargs["load_in_8bit"] = True
+    elif load_4bit:
+        kwargs["load_in_4bit"] = True
+        kwargs["quantization_config"] = BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.bfloat16 if load_bf16 else torch.float16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
+        )
+    if load_bf16:
+        kwargs["torch_dtype"] = torch.bfloat16
+    else:
+        kwargs["torch_dtype"] = torch.float16
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
+    model = LlavaLlamaForCausalLM.from_pretrained(
+        model_name, low_cpu_mem_usage=True, **kwargs
+    )
+
+    mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
+    tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
+    if mm_use_im_start_end:
+        tokenizer.add_tokens(
+            [DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True
+        )
+
+    vision_tower = model.model.vision_tower[0]
+    if vision_tower.device.type == 'meta':
+        vision_tower = CLIPVisionModel.from_pretrained(vision_tower.config._name_or_path, torch_dtype=torch.bfloat16 if load_bf16 else torch.float16, low_cpu_mem_usage=True).cuda()
+        model.model.vision_tower[0] = vision_tower
+    else:
+        vision_tower.to(device="cuda", dtype=torch.bfloat16 if load_bf16 else torch.float16)
+
+    vision_config = vision_tower.config
+    vision_config.im_patch_token = tokenizer.convert_tokens_to_ids(
+        [DEFAULT_IMAGE_PATCH_TOKEN]
+    )[0]
+    vision_config.use_im_start_end = mm_use_im_start_end
+    if mm_use_im_start_end:
+        (
+            vision_config.im_start_token,
+            vision_config.im_end_token,
+        ) = tokenizer.convert_tokens_to_ids(
+            [DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN]
+        )
+    image_processor = CLIPImageProcessor.from_pretrained(model.config.mm_vision_tower, torch_dtype=torch.bfloat16 if load_bf16 else torch.float16)
+    image_token_len = (vision_config.image_size // vision_config.patch_size) ** 2
+    return model, tokenizer, image_processor, image_token_len
 
 
 def patch_config(config):
@@ -66,36 +131,10 @@ def eval_model(args):
     patch_config(model_name)
 
     print(model_name)
-    model = LlavaLlamaForCausalLM.from_pretrained(
-        model_name, torch_dtype=torch.float16, use_cache=True
-    ).cuda()
-    image_processor = CLIPImageProcessor.from_pretrained(
-        model.config.mm_vision_tower, torch_dtype=torch.float16
+
+    model, tokenizer, image_processor, image_token_len = load_pretrained_model(
+        model_name
     )
-    vision_tower = model.model.vision_tower[0]
-    vision_tower.to(device="cuda", dtype=torch.float16)
-
-    mm_use_im_start_end = getattr(model.config, "mm_use_im_start_end", False)
-    tokenizer.add_tokens([DEFAULT_IMAGE_PATCH_TOKEN], special_tokens=True)
-    if mm_use_im_start_end:
-        tokenizer.add_tokens(
-            [DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN], special_tokens=True
-        )
-
-    # import pdb; pdb.set_trace()
-    vision_config = vision_tower.config
-    vision_config.im_patch_token = tokenizer.convert_tokens_to_ids(
-        [DEFAULT_IMAGE_PATCH_TOKEN]
-    )[0]
-    vision_config.use_im_start_end = mm_use_im_start_end
-    if mm_use_im_start_end:
-        (
-            vision_config.im_start_token,
-            vision_config.im_end_token,
-        ) = tokenizer.convert_tokens_to_ids(
-            [DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN]
-        )
-    image_token_len = (vision_config.image_size // vision_config.patch_size) ** 2
 
     print(f"Loading dataset: {args.dataset} ({args.split})")
     dataset = load_dataset(args.dataset)
@@ -145,6 +184,7 @@ def eval_model(args):
 
             conv = conv_templates["multimodal"].copy()
             conv.append_message(conv.roles[0], qs)
+            conv.append_message(conv.roles[1], None)
             prompt = conv.get_prompt()
             inputs = tokenizer([prompt])
 
