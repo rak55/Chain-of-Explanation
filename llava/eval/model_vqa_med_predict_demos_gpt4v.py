629b7134060546ff391d0f000361efe2cb084fa9
Max Weinzierl <supermaxman1@gmail.com>
2023-12-05 14:47:58 -0600

    Added GPT-4 pred

--- llava/eval/model_vqa_med_predict_demos_gpt4v.py
+++ llava/eval/model_vqa_med_predict_demos_gpt4v.py
@@ -126,6 +126,69 @@ def print_messages(messages):
         print()
 
 
+@dataclasses.dataclass
+class CompletionUsageEstimate:
+    completion_tokens: int
+    prompt_tokens: int
+    completion_cost: float
+    prompt_cost: float
+    total_cost: float
+
+
+class CompletionUsageEstimator:
+    def __init__(
+        self,
+        completion_cost: float = 0.03,
+        completion_cost_tokens: int = 1000,
+        prompt_cost: float = 0.01,
+        prompt_cost_tokens: int = 1000,
+    ):
+        self.config = config
+        self.running_completion_tokens = 0
+        self.running_prompt_tokens = 0
+        self.prompts = 0
+        self.remaining_prompts = 0
+        self.completion_cost = completion_cost / completion_cost_tokens
+        self.prompt_cost = prompt_cost / prompt_cost_tokens
+
+    def init(self, remaining_prompts: int):
+        self.running_completion_tokens = 0
+        self.running_prompt_tokens = 0
+        self.prompts = 0
+        self.remaining_prompts = remaining_prompts
+
+    def update(self, completion: ChatCompletion):
+        usage = completion.usage
+        if usage is not None:
+            self.running_completion_tokens += usage.completion_tokens
+            self.running_prompt_tokens += usage.prompt_tokens
+            self.prompts += 1
+            self.remaining_prompts -= 1
+
+    def estimate(self):
+        # linearly interpolate by computing average tokens per prompt and multiplying by remaining prompts
+        completion_tokens_per_prompt = self.running_completion_tokens / self.prompts
+        prompt_tokens_per_prompt = self.running_prompt_tokens / self.prompts
+        total_completion_tokens = (
+            self.running_completion_tokens
+            + self.remaining_prompts * completion_tokens_per_prompt
+        )
+        total_prompt_tokens = (
+            self.running_prompt_tokens
+            + self.remaining_prompts * prompt_tokens_per_prompt
+        )
+        completion_cost = total_completion_tokens * self.completion_cost
+        prompt_cost = total_prompt_tokens * self.prompt_cost
+        total_cost = completion_cost + prompt_cost
+        return CompletionUsageEstimate(
+            completion_tokens=total_completion_tokens,
+            prompt_tokens=total_prompt_tokens,
+            completion_cost=completion_cost,
+            prompt_cost=prompt_cost,
+            total_cost=total_cost,
+        )
+
+
 def eval_model(
     config: Config,
     dataset_name: str,
@@ -155,42 +218,54 @@ def eval_model(
                 line = json.loads(line)
                 seen_ids.add(line["id"])
 
+    usage = CompletionUsageEstimator()
+    usage.init(len(data_split) - len(seen_ids))
+
     with open(answers_file, "a") as f:
-        for idx in tqdm(range(len(data_split))):
-            if idx in seen_ids:
-                continue
-            ex = data_split[idx]
-
-            messages = creator.create_context(idx, ex)
-            # print_messages(messages)
-            with MinimumDelay(config.delay):
-                completion = chat(
-                    client,
-                    model=config.model,
-                    messages=messages,
-                    max_tokens=config.max_tokens,
-                    temperature=config.temperature,
-                    top_p=config.top_p,
-                    seed=config.seed,
-                )
-                if completion is None:
+        with tqdm(total=len(data_split)) as pbar:
+            pbar.update(len(seen_ids))
+            for idx in range(len(data_split)):
+                if idx in seen_ids:
                     continue
-                content = completion.choices[0].message.content
+                ex = data_split[idx]
 
-            messages.append({"role": "assistant", "content": content})
-            # print_messages([messages[-1]])
-            # print("---")
+                messages = creator.create_context(idx, ex)
+                # print_messages(messages)
+                with MinimumDelay(config.delay):
+                    completion = chat(
+                        client,
+                        model=config.model,
+                        messages=messages,
+                        max_tokens=config.max_tokens,
+                        temperature=config.temperature,
+                        top_p=config.top_p,
+                        seed=config.seed,
+                    )
+                    if completion is None:
+                        continue
+                    content = completion.choices[0].message.content
+                    usage.update(completion)
+                messages.append({"role": "assistant", "content": content})
+                # print_messages([messages[-1]])
+                # print("---")
+                use = usage.estimate()
 
-            f.write(
-                json.dumps(
+                f.write(
+                    json.dumps(
+                        {
+                            "id": idx,
+                            "rationale": "",
+                            "pred": content,
+                        }
+                    )
+                    + "\n"
+                )
+                pbar.update(1)
+                pbar.set_postfix(
                     {
-                        "id": idx,
-                        "rationale": "",
-                        "pred": content,
+                        "cost": f"${use.total_cost:.2f}",
                     }
                 )
-                + "\n"
-            )
 
 
 if __name__ == "__main__":
