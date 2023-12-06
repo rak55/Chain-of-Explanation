dee3f339abf86a85d862aeb4f56572a0149761be
Max Weinzierl <maxwellweinzierl@gmail.com>
2023-12-06 16:33:00 -0600

    Added experiments

---
+++ llava/eval/experiments.sh
@@ -0,0 +1,5 @@
+
+python llava/eval/model_vqa_med_predict_gpt4v.py \
+    --config configs/gpt-4v-pred.yaml \
+    --dataset flaviagiammarino/vqa-rad \
+    --output /shared/aifiles/disk1/media/artifacts/LLaVA-Med/VQA-RAD/train-predictions-gpt4v.jsonl
