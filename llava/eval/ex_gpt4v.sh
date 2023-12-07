bb5849d39b33b9e9716d0af6e688c6f8d17b4ba8
Max Weinzierl <supermaxman1@gmail.com>
2023-12-07 03:25:08 -0600

    Added GPT-4 pred

--- llava/eval/ex_gpt4v.sh
+++ llava/eval/ex_gpt4v.sh
@@ -4,14 +4,18 @@
 BASE_DIR=/shared/aifiles/disk1/media/artifacts/LLaVA-Med/VQA-RAD
 
 declare -a search=("image" "question")
-# declare -a index=("rationale" "image" "question")
-declare -a index=("image" "question")
+declare -a index=("rationale" "image" "question")
 
 for s in "${search[@]}"
 do
     for i in "${index[@]}"
     do
         echo "Search mode: $s, Index mode: $i"
+        # skip $s=image, $i=rationale because already done
+        if [ "$s" == "image" ] && [ "$i" == "rationale" ]; then
+            echo "Skipping"
+            continue
+        fi
         python llava/eval/model_vqa_med_predict_demos_gpt4v.py \
             --config configs/gpt-4v-3.yaml \
             --dataset flaviagiammarino/vqa-rad \
