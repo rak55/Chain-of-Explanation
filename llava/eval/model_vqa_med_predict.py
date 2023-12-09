5027fab43e01d71cc2902c034e06a0b14723284d
Max Weinzierl <supermaxman1@gmail.com>
2023-12-09 13:26:41 -0600

    Added local SLAKE

--- llava/eval/model_vqa_med_predict.py
+++ llava/eval/model_vqa_med_predict.py
@@ -197,79 +197,79 @@ def eval_model(args):
                 line = json.loads(line)
                 seen_ids.add(line["id"])
 
-    with open(answers_file, "a") as f:
-        for idx in tqdm(range(len(data_split))):
-            if idx in seen_ids:
-                continue
-            ex = data_split[idx]
-            image = ex["image"]
-            question = ex["question"]
-            answer = ex["answer"]
-            conv = conv_templates["multimodal"].copy()
+    for idx in tqdm(range(len(data_split))):
+        if idx in seen_ids:
+            continue
+        ex = data_split[idx]
+        image = ex["image"]
+        question = ex["question"]
+        answer = ex["answer"]
+        conv = conv_templates["multimodal"].copy()
 
-            qs = question
+        qs = question
 
-            image_tensor = image_processor.preprocess(image, return_tensors="pt")[
-                "pixel_values"
-            ][0]
-            images = image_tensor.unsqueeze(0).half().cuda()
-            if getattr(model.config, "mm_use_im_start_end", False):
-                qs = (
-                    qs
-                    + "\n"
-                    + DEFAULT_IM_START_TOKEN
-                    + DEFAULT_IMAGE_PATCH_TOKEN * image_token_len
-                    + DEFAULT_IM_END_TOKEN
-                )
-            else:
-                qs = qs + "\n" + DEFAULT_IMAGE_PATCH_TOKEN * image_token_len
-
-            conv.append_message(conv.roles[0], qs)
-            conv.append_message(conv.roles[1], answer)
-
-            qs = "Explain why this answer is correct in great detail, referencing the provided image. Think step-by-step, and make sure to only draw conclusions from evidence present in the above image."
-
-            conv.append_message(conv.roles[0], qs)
-            conv.append_message(conv.roles[1], None)
-
-            prompt = conv.get_prompt()
-            inputs = tokenizer([prompt])
-
-            input_ids = torch.as_tensor(inputs.input_ids).cuda()
-
-            keywords = ["###"]
-            stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
-
-            with torch.inference_mode():
-                output_ids = model.generate(
-                    input_ids,
-                    images=images,
-                    do_sample=True,
-                    top_p=0.7,
-                    max_new_tokens=1024,
-                    stopping_criteria=[stopping_criteria],
-                )
-
-            input_token_len = input_ids.shape[1]
-            n_diff_input_output = (
-                (input_ids != output_ids[:, :input_token_len]).sum().item()
+        image_tensor = image_processor.preprocess(image, return_tensors="pt")[
+            "pixel_values"
+        ][0]
+        images = image_tensor.unsqueeze(0).half().cuda()
+        if getattr(model.config, "mm_use_im_start_end", False):
+            qs = (
+                qs
+                + "\n"
+                + DEFAULT_IM_START_TOKEN
+                + DEFAULT_IMAGE_PATCH_TOKEN * image_token_len
+                + DEFAULT_IM_END_TOKEN
             )
-            if n_diff_input_output > 0:
-                print(
-                    f"[Warning] Sample {idx}: {n_diff_input_output} output_ids are not the same as the input_ids"
-                )
-            outputs = tokenizer.batch_decode(
-                output_ids[:, input_token_len:], skip_special_tokens=True
-            )[0]
+        else:
+            qs = qs + "\n" + DEFAULT_IMAGE_PATCH_TOKEN * image_token_len
 
-            try:
-                index = outputs.index(conv.sep)
-            except ValueError:
-                outputs += conv.sep
-                index = outputs.index(conv.sep)
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], answer)
 
-            outputs = outputs[:index].strip()
+        qs = "Explain why this answer is correct in great detail, referencing the provided image. Think step-by-step, and make sure to only draw conclusions from evidence present in the above image."
 
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+
+        prompt = conv.get_prompt()
+        inputs = tokenizer([prompt])
+
+        input_ids = torch.as_tensor(inputs.input_ids).cuda()
+
+        keywords = ["###"]
+        stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
+
+        with torch.inference_mode():
+            output_ids = model.generate(
+                input_ids,
+                images=images,
+                do_sample=True,
+                top_p=0.7,
+                max_new_tokens=1024,
+                stopping_criteria=[stopping_criteria],
+            )
+
+        input_token_len = input_ids.shape[1]
+        n_diff_input_output = (
+            (input_ids != output_ids[:, :input_token_len]).sum().item()
+        )
+        if n_diff_input_output > 0:
+            print(
+                f"[Warning] Sample {idx}: {n_diff_input_output} output_ids are not the same as the input_ids"
+            )
+        outputs = tokenizer.batch_decode(
+            output_ids[:, input_token_len:], skip_special_tokens=True
+        )[0]
+
+        try:
+            index = outputs.index(conv.sep)
+        except ValueError:
+            outputs += conv.sep
+            index = outputs.index(conv.sep)
+
+        outputs = outputs[:index].strip()
+
+        with open(answers_file, "a") as f:
             f.write(
                 json.dumps(
                     {
